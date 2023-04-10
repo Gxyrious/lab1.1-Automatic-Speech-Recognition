6e6efeff1e06be965d1556e88e2295165390f615
Gxyrious <2054164@tongji.edu.cn>
2023-04-10 19:46:35 +0800

    初步

--- asr.py
+++ asr.py
@@ -14,7 +14,7 @@ class myWindow(QtWidgets.QMainWindow):
         super(myWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.start_btn.clicked.connect(partial(self.getCommand))
+        self.ui.speak_btn.clicked.connect(partial(self.getCommand))
         self.score_threshold = 80
         self.commands = {"open /Applications/WeChat.app": "play music", "open txt": "txt"}
 
@@ -75,10 +75,10 @@ class myWindow(QtWidgets.QMainWindow):
         return response
     
     def getCommand(self):
-        # microphone = sr.Microphone()
+        microphone = sr.Microphone()
         recognizer = sr.Recognizer()
         for i in range(5):
-            order = self.recognize_speech_from_audio(recognizer, "audio.wav")
+            order = self.recognize_speech_from_mic(recognizer, microphone)
             if order["transcription"]:
                 break
             if not order["success"]:
@@ -91,8 +91,34 @@ class myWindow(QtWidgets.QMainWindow):
         self.myCommand = order["transcription"]
         key, score, cmd = process.extractOne(self.myCommand, self.commands)
         print(key, score, cmd)
+        self.showCommand()
+        
+    
+    def showCommand(self):
+        showMessage = QtWidgets.QMessageBox()
+        showMessage.setWindowTitle("提示")
+        showMessage.setText("You said: {}".format(self.myCommand))
+        buttonY = QtWidgets.QPushButton('Yes')
+        buttonN = QtWidgets.QPushButton('No')
+        showMessage.addButton(buttonY, QtWidgets.QMessageBox.YesRole)
+        showMessage.addButton(buttonN, QtWidgets.QMessageBox.NoRole)
+        showMessage.exec_()
+        if showMessage.clickedButton() == buttonY:
+            self.execCommand()
+    
+    def execCommand(self):
+        key, score, cmd = process.extractOne(self.myCommand, self.commands)
         if score > self.score_threshold:
             os.system(cmd)
+        else:
+            # 提示无法识别
+            showMessage = QtWidgets.QMessageBox()
+            showMessage.setWindowTitle("警告")
+            showMessage.setText("Sorry, I can't recognize your command.")
+            buttonY = QtWidgets.QPushButton('OK')
+            showMessage.addButton(buttonY, QtWidgets.QMessageBox.YesRole)
+            showMessage.exec_()
+            
 
 app = QtWidgets.QApplication([])
 application = myWindow()
